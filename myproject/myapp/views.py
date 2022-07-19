ff648b65a1ad52076b7b55bf1034057b57056f98
ahmadiaon <dataahmadi2020@gmail.com>
2022-07-19 07:42:30 +0700

    single variable

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -4,5 +4,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
+    name = "ini nama orang"
     # return HttpResponse('<h1>Hello Mitra Barito</h1>')
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'name' : name})
